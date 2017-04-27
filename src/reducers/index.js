b3facb17bb7facf71b484dba88d5824e9e1fe8bc
Kevin Lam <kevinlam.ucsb@gmail.com>
2017-04-26 17:46:23 -0700

    took out redux forms

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
 import loginReducer from './login';
 
 const rootReducer = combineReducers({
-  form: formReducer,
   login: loginReducer
 });
 
