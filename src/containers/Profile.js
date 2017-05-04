c5a919d3b1917e3e6326d5b9e2890c7a89afc338
Paola <paolita.claros@gmail.com>
2017-05-03 22:09:40 -0700

    adding style to profile component

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,54 +1,100 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Avatar, Grid, Row } from 'react-native-elements';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { Avatar, Grid, Row, Tile, Col } from 'react-native-elements';
 
 import { Button, CardSection, Card, Input, Spinner } from '../components/common';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 0,
-    flexDirection: 'row',
-    // height: 200,
-    // width: 200,
+    flex: 1,
+    flexDirection: 'column',
+    // height: 800,
+    // width: 800,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#58FE58'
   },
   avatarProfile: {
     flex: 0,
+    borderRadius: 50,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
-    height: 150,
-    width: 150,
+    justifyContent: 'space-around',
+    height: 82,
+    width: 82,
     backgroundColor: 'red'
+  },
+  imgContainer: {
+    flex: 1
+  },
+  imageInProf: {
+    width: 122,
+    height: 122
+  },
+  titleText: {
+    fontSize: 40,
+    fontWeight: 'bold'
   }
 });
 
 const Profile = () => {
   return (
-    // <Card>
-    //   <CardSection>
-    //         <Avatar style={styles.container}
-    //           xlarge
-    //           rounded
-    //           source={{uri: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg"}}
-    //           // onPress={() => console.log("Works!")}
-    //           activeOpacity={0.7}
-    //         />
-    //   </CardSection>
-    // </Card>
-    <Grid>
-      <Row style={styles.container}>
-        <Avatar
-          style={styles.avatarProfile}
-          xlarge
-          rounded
-          source={{uri: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg"}}
-          activeOpacity={0.7}
-        />
-      </Row>
-    </Grid>
+    <View>
+      <CardSection>
+        <Card>
+          <Image
+            style={styles.avatarProfile}
+            source={{uri: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg"}}
+          />
+        </Card>
+        <Card>
+        <View style={styles.container}>
+            <Text style={styles.titleText}>Mary Lai</Text>
+            <Text >Push Ups: 40</Text>
+        </View>
+      </Card>
+      </CardSection>
+      <CardSection>
+        <Card style={styles.imgContainer}>
+          <Image
+            style={styles.imageInProf}
+            source={{ uri: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg" }}
+          />
+        </Card>
+        <Card style={styles.imgContainer}>
+          <Image
+            style={styles.imageInProf}
+            source={{ uri: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg" }}
+          />
+        </Card>
+        <Card style={styles.imgContainer}>
+          <Image
+            style={styles.imageInProf}
+            source={{ uri: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg" }}
+          />
+        </Card>
+      </CardSection>
+      <CardSection>
+        <Card style={styles.imgContainer}>
+          <Image
+            style={styles.imageInProf}
+            source={{ uri: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg" }}
+          />
+        </Card>
+        <Card style={styles.imgContainer}>
+          <Image
+            style={styles.imageInProf}
+            source={{ uri: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg" }}
+          />
+        </Card>
+        <Card style={styles.imgContainer}>
+          <Image
+            style={styles.imageInProf}
+            source={{ uri: "https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg" }}
+          />
+        </Card>
+      </CardSection>
+    </View>
   )
 };
 
