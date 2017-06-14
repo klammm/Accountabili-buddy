083367389fb7b24626de91f36fbdc022fa9b5016
Paola <paolita.claros@gmail.com>
2017-06-14 11:19:33 -0700

    added caption to images

--- src/components/common/CardSection.js
+++ src/components/common/CardSection.js
@@ -13,7 +13,7 @@ const styles = StyleSheet.create({
   containerStyle: {
     borderBottomWidth: 1,
     padding: 5,
-    backgroundColor: '#ddd',
+    backgroundColor: '#fff',
     justifyContent: 'flex-start',
     flexDirection: 'row',
     borderColor: '#ddd',
